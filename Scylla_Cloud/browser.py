bab0e353a06d4166cf66f487f99232e8023778d9
Anastasia89038 <59653663+Anastasia89038@users.noreply.github.com>
2022-10-04 21:03:52 +0300

    добавлен простенький тест на вменяемость

--- Scylla_Cloud/browser.py
+++ Scylla_Cloud/browser.py
@@ -4,12 +4,16 @@ class Browser:
     def __init__(self, base_url="https://cloud.scylladb.com/user/signup"):
         self.driver = webdriver.Chrome()
         self.base_url = base_url
-        self.driver.get(self.base_url)
+
         #self.driver.implicitly_wait(5)
+    def go_to_site(self):
+        return self.driver.get(self.base_url)
     # @property
     # def current_url(self):
     #     return self.driver.current_url
+    def get_driver(self):
+        return self.driver
 
     def close_browser(self):
-        self.driver.quit()
+        return self.driver.quit()
 
--- Scylla_Cloud/pages/signUp/__init__.py
+++ Scylla_Cloud/pages/signUp/__init__.py
@@ -28,22 +28,22 @@ class ButtonElement(Element):
 
 class SignUp():
 
-    def __init__(self, base_url="https://cloud.scylladb.com/user/signup"):
+        def __init__(self, base_url="https://cloud.scylladb.com/user/signup"):
        # self.path = "/user/signup"
       #  self.url = base_url + self.path
-        self.driver = webdriver.Chrome()
-        self.base_url = base_url
-        self.driver.get(self.base_url)
-        self.first_name = InputElement(self.driver, locators.first_name_locator)
-        self.last_name = InputElement(self.driver, locators.last_name_locator)
-        self.email = InputElement(self.driver, locators.email_locator)
-        self.password = InputElement(self.driver, locators.password_locator)
-        self.company = InputElement(self.driver, locators.company_locator)
-     #   self.country = InputElement(self.driver, locators.country_locator)
-        self.phone = InputElement(self.driver, locators.phone_locator)
-        self.agreement_check_box = CheckBoxElement(self.driver, locators.agreement_check_box_locator)
-        self.signup_button = ButtonElement(self.driver, locators.signup_button_locator)
+            browser = Browser()
+            browser.go_to_site()
+            browser.get_driver().implicitly_wait(5)
+            self.first_name = InputElement(browser.get_driver(), locators.first_name_locator)
+            self.last_name = InputElement(browser.get_driver(), locators.last_name_locator)
+            self.email = InputElement(browser.get_driver(), locators.email_locator)
+            self.password = InputElement(browser.get_driver(), locators.password_locator)
+            self.company = InputElement(browser.get_driver(), locators.company_locator)
+            self.country = InputElement(browser.driver, locators.country_locator)
+            self.phone = InputElement(browser.get_driver(), locators.phone_locator)
+            self.agreement_check_box = CheckBoxElement(browser.get_driver(), locators.agreement_check_box_locator)
+            self.signup_button = ButtonElement(browser.get_driver(), locators.signup_button_locator)
 
-    def signup(self):
-        pass
+        def signup(self):
+            pass
 
