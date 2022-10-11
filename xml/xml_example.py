8747a6752d20b0c4f0caf92fd23f5e7bd2f0ad2b
Anastasia89038 <59653663+Anastasia89038@users.noreply.github.com>
2022-10-11 21:20:35 +0300

    создан парсер xml

--- xml/xml_example.py
+++ xml/xml_example.py
@@ -7,6 +7,7 @@ class Person:
         self.birthday_year = birthday_year
         self.phone_code = phone_code
         self.phone_number = phone_number
+
     def dump_to_xml(self):
         person = ET.Element("person")#создаем тег (или ветку) <person>
         name = ET.SubElement(person, "name")#создаем тег (или ветку) <name>, которая лежит в <person>
@@ -21,6 +22,25 @@ class Person:
         phone_number = ET.SubElement(phone, "phone_number")
         phone_number.text = str(self.phone_number)
         return person
+
+    @classmethod
+    def parse_xml(cls, xml_element):
+        atrributes = {atrribute.tag: atrribute.text for atrribute in xml_element}
+        name = atrributes["name"]
+        last_name = atrributes["last_name"]
+        birthday_year = atrributes["birthday_year"]
+        for atrribute in xml_element:
+            if atrribute.tag == "phone":
+                atrributes_phone = {atrribute[0].tag: atrribute[0].text, atrribute[1].tag: atrribute[1].text}
+        phone_code = int(atrributes_phone["phone_code"])
+        phone_number = int(atrributes_phone["phone_number"])
+        return cls(name=name, last_name=last_name, birthday_year=birthday_year, phone_code=phone_code, phone_number=phone_number)
+
+    def __str__(self):
+        return f"name:{self.name}\n last name: {self.last_name}\nbirthday_year: {self.birthday_year}"
+
+    def __repr__(self):
+        return self.__str__()
 
 def save_department(file_name):
     person_Ivan = Person("Ivan", "Ivanov", 1980, 933, 5550000)
@@ -40,6 +60,12 @@ def save_department(file_name):
 def main():
     department_file_name = "department.xml"
     save_department(department_file_name)
-
+    person_Ivan = Person("name", "last_name", 1980, 933, 5550000)
+    person_Ivan_xml = person_Ivan.dump_to_xml()
+    person = Person("", "", 0, 0, 0)
+    person = person.parse_xml(person_Ivan_xml)
+    print(person)
+    print(person.name)
+    print(person.phone_code)
 if __name__ == '__main__':
    main()
